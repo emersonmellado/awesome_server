9352a89080d9a0cf70fe2ed5ced624b7b7862337
Emerson Mellado <emersonmellado@gmail.com>
2021-04-05 18:38:31 -0700

    making font blue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello hello from Awesome server")
+    res.send("<h1 style='color: blue;'>Hello hello from Awesome server</h1>")
 })
 
 app.listen(port, () => {
