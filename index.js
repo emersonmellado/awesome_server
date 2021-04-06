c2ef9aa6d9ebc392e9a6e56225a0bbff9ce57add
Emerson Mellado <emersonmellado@gmail.com>
2021-04-05 18:43:16 -0700

    adding categories routes

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+const categoryRoutes = require('./routes/categories')
+
 app.get('/', (req, res) => {
     res.send("<h1 style='color: blue;'>Hello hello from Awesome server</h1>")
 })
+
+app.use('/categories', categoryRoutes);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
